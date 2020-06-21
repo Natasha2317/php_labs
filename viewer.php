724e7a372771602eb0dceae16a19b8e386dba03b
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2020-06-21 14:29:03 +0300

    Update viewer.php

--- viewer.php
+++ viewer.php
@@ -1,13 +1,13 @@
 <?php
 
     // Параметры для подключения
-    $mysqli = new mysqli('localhost', 'root', '', 'tel_book');
+    $mysqli = new mysqli('std-mysql', 'std_946', 'cisco12345', 'std_946');
 
     if (mysqli_connect_error()){
         prinf("Соединение не установлено", mysqli_connect_error());
         exit();
     }
-    $query = $mysqli->query('SELECT * FROM contacts');
+    $query = $mysqli->query('SELECT * FROM poems');
 
         while ($row = mysqli_fetch_assoc($query)){
             echo $row['fam'].'<br>'.$row['name'].'<br>'.$row['otсhestvo'].'<br>'.$row['pol'].'<br>'.$row['date_birthday'].'<br>'.$row['phone'].'<br>'.$row['address'].'<br>'.$row['email'].'<br>'.$row['comment'];
