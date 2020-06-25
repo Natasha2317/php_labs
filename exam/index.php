dcb291d320f53a4a4f770c2fb159072a3a44f114
Natasha2317 <56071335+Natasha2317@users.noreply.github.com>
2020-06-25 16:15:56 +0300

    Update index.php

--- exam/index.php
+++ exam/index.php
@@ -34,7 +34,7 @@
         $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
 
         if ($password == '12345'){
-        $url = 'http://localhost/exam/create.php';
+        $url = 'http://php-exam.std-946.ist.mospolytech.ru/exam/create.php';
 
         header("Location: $url");
         }
